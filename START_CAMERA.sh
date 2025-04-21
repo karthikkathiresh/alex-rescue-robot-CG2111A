4d691e0f79f67ea7559a9f238d5bce81f735b0ff
Karthik Kathiresh <167991041+karthikkathiresh@users.noreply.github.com>
2025-04-21 21:05:42 +0800

    Add files via upload

---
+++ START_CAMERA.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ./camera/alex/env/bin/activate
+python ./camera/alex/labs/Camera/AlexCameraStreamServer.py
---
+++ START_SLAM.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ./slamcamera/alex/env/bin/activate
+python ./slamcamera/alex/labs/SlamLab/alex_main.py
